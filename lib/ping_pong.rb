29cc9ed3a1d56400a980b1a424676ffb416dd134
Braden Rich <bradenwrich@gmail.com>
2022-02-23 15:59:42 -0500

    Implement useful links

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -7,6 +7,7 @@ class PingPong
 
   #https://ruby-doc.org/core-2.7.0/Array.html#method-i-map was used for .map usage
   #https://www.rubyguides.com/ruby-tutorial/loops/ was used for if, elsif, else loop
+  #https://www.codewars.com/kata/5545f109004975ea66000086 - a prior codewar kata that I used my previous code to help solve this!
   def play(number)
     Array(1..number).map do |x|
       if x % 3 == 0 && x % 5 == 0 
